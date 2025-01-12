8e67d259cda6eadeac029276bb3132dfd728efcf
David Abell <79927957+david-abell@users.noreply.github.com>
2025-01-12 11:04:11 +0000

    add actions build/deploy workflows

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/give-me-mah-hours",
 };
 
 export default nextConfig;
